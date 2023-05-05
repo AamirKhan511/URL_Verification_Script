88f3784f32b4cac4066aea5962736157de141715
Waqas Zeeshan <w.zeshan@itg.com.kw>
2023-05-05 12:43:00 +0500

    adding new changes

--- verifyurl.sh
+++ verifyurl.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
-file="./textfile.txt"
+file="/home/m.khan/URL_Verification_Script/textfile.txt"
 url=$(grep 'http' $file | cut -d "'" -f 4 | sed s/'URL:\/\/'/''/g)
 END=3
 regex='^(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]\.[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$'
+
 
 IFS=$'\n'       # make newlines the only separator
 for j in $url
   do
 	if [[ $j =~ $regex ]]
-       then
-        echo "$j IS valid"
-      else
+      then
+       	  for i in $(seq 1 $END); do
+                if curl -s --head --request GET "$j" | grep "200 OK" > /dev/null; then                     
+                    echo $j "is UP"
+                    curl -k POST -H 'Content-type: application/json' --data '{"text":"'"$j is UP."'"}' https://hooks.slack.com/services/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
+                 else
+                    http_response=$(curl -s -o response.txt -w "%{http_code}" $j)
+                    if [ $http_response != "200" ]; then
+                       echo $j "is DOWN responce code is" $http_response
+                       curl -k POST -H 'Content-type: application/json' --data '{"text":"'"$j is Down. Responce code: $http_response"'"}' https://hooks.slack.com/services/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
+                    fi
+                fi    
+            done
+     else
         echo "$j IS NOT valid"
     fi
- if curl -s --head --request GET "$j" | grep "200 OK" > /dev/null; then
-   echo $j "is UP"
-   curl -k POST -H 'Content-type: application/json' --data '{"text":"'"$j is UP."'"}' https://hooks.slack.com/services/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
- else
-  for i in $(seq 1 $END); do
-    if curl -s --head --request GET "$j" | grep "200 OK" > /dev/null; then
-      echo $j "is UP"
-    fi
-  done
-  http_response=$(curl -s -o response.txt -w "%{http_code}" $j)
-   if [ $http_response != "200" ]; then
-    echo $j "is DOWN responce code is" $http_response
-    curl -k POST -H 'Content-type: application/json' --data '{"text":"'"$j is Down. Responce code: $http_response"'"}' https://hooks.slack.com/services/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
-   fi
-fi 
 done
